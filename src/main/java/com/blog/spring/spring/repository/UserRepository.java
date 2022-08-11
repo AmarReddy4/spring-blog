448b9c54404f8e227b9042327a41b88ab03f3f01
Amarendar Reddy <amarendarkanthala@gmail.com>
2022-08-10 22:06:59 -0400

    Initial commit

---
+++ src/main/java/com/blog/spring/spring/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.blog.spring.spring.repository;
+
+import com.blog.spring.spring.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+}
---
+++ src/main/java/com/blog/spring/spring/service/AuthService.java
@@ -0,0 +1,45 @@
+package com.blog.spring.spring.service;
+
+import com.blog.spring.spring.dto.LoginRequest;
+import com.blog.spring.spring.dto.RegisterRequest;
+import com.blog.spring.spring.model.User;
+import com.blog.spring.spring.repository.UserRepository;
+import com.blog.spring.spring.security.JwtProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtProvider jwtProvider;
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUserName(registerRequest.getUsername());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
+        user.setEmail(registerRequest.getEmail());
+        userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    public String login(LoginRequest loginRequest) {
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
+                loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        return jwtProvider.generateToken(authenticate);
+    }
+}
